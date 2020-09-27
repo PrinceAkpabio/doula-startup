e015172b18abe6744dc7cff65a1571dd5f8c8df9
prince <princeakpabio90@gmail.com>
2020-09-27 22:14:48 +0100

    added about section

---
+++ src/components/about/about.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './about.scss';
+
+const About = () => (
+ <>
+  <section class="about" id="about">
+   <div class="about-image"></div>
+
+   <div class="about-text">
+    <h1>Hi, I'm Louise Rowland</h1>
+    <h3>VIRTUAL ASSISTANT TO PASSIONATE ONLINE BUSINESS OWNERS</h3>
+    <p>Topping liquorice sesame snaps cookie sesame snaps. Sweet jujubes tootsie roll chocolate marshmallow jelly-o jelly
+    beans. Sesame snaps oat cake tiramisu sweet roll. Halvah icing cotton candy. Pie croissant biscuit. Muffin donut muffin
+    topping danish wafer cake. Topping liquorice sesame snaps cookie sesame snaps. Sweet jujubes tootsie roll chocolate
+  marshmallow jelly-o jelly beans.</p>
+
+    <button id="aboutBtn">Get to know me</button>
+   </div>
+   {/* <div class="image-line"></div>  */}
+</section>
+
+ </>
+);
+
+export default About;
