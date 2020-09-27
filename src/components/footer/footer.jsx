9ef5121e149906d7e62eb1e27ecff3885e86eeb0
prince <princeakpabio90@gmail.com>
2020-09-27 15:24:42 +0100

    added responsive footer feature

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './footer.scss';
+
+
+const Footer = () => (
+  <>
+    <footer id="footer">
+      <h4> DOULA STARTUP | DEVELOPED BY BOOST YOUR BUSINESS &copy; 2020</h4>
+    </footer>
+  </>
+);
+
+export default Footer;
