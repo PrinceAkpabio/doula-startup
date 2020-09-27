0a52e86daac934d4d2aad05c91ba6b0493d2eb00
prince <princeakpabio90@gmail.com>
2020-09-27 21:49:13 +0100

    added into section

---
+++ src/components/intro/intro.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './intro.scss';
+
+
+const Intro = () =>( 
+ <>
+  <section class="intro">
+   <div class="intro_text">
+    <h2>ARE YOU READY TO HAND OVER THE BEHIND THE SCENES PARTS OF YOUR BUSINESS SO YOU CAN CONCENTRATE ON WHAT YOU DO BEST?</h2>
+
+    <p>Topping liquorice sesame snaps cookie sesame snaps. Sweet jujubes tootsie roll chocolate marshmallow jelly-o jelly
+    beans. Sesame snaps oat cake tiramisu sweet roll. Halvah icing cotton candy. Pie croissant biscuit. Muffin donut muffin
+  topping danish wafer cake. Topping liquorice sesame snaps cookie sesame snaps.</p>
+   </div>
+  </section>
+ </>
+);
+
+export default Intro;
