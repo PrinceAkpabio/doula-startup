d856c43954d1b3ead7c3e37e92a81c3d7eab8a58
prince <princeakpabio90@gmail.com>
2020-09-28 18:17:39 +0100

    added responsive navbar with animated toggle button

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -6,25 +6,80 @@ import './nav-bar.scss';
 const NavBar = () => {
   const [menu, setMenu] = useState();
 
-  const openMenu = () => {
+  const toggleMenu = () => {
     setMenu(prevMenu => !prevMenu)
   }
 
-  const closeMenu = () => {
-    setMenu(nextMenu => !nextMenu
-    )
-  }
+  // const closeMenu = () => {
+  //   setMenu(nextMenu => !nextMenu
+  //   )
+  // }
     return (
       <>
-
-        {/* Nav Bar */}
-        <nav id="navbar">
+        <header className="header">
           <Link to="#" className="logo-link">
-            <div className="logo"></div>
+            <div className="logo mobile"></div>
           </Link>
 
-          {/* Tablet and Large Screen Menus */}
-          <div className="lg-nav-menu">
+          <div className="menuBtn" onClick={toggleMenu}>
+            <span
+              id="menuBtn_hamburger1"
+              className={` ${menu ? "" : "open"}`}
+            ></span>
+
+            <span
+              id="menuBtn_hamburger2"
+              className={` ${menu ? "" : "open"}`}
+            ></span>
+            <span
+              id="menuBtn_hamburger3"
+              className={` ${menu ? "" : "open"}`}
+            ></span>
+          </div>
+
+          <nav className={`nav ${menu ? "" : "open"}`}>
+            <ul className={`menu-nav ${menu ? "" : "open"}`}>
+              <Link to="#" className="logo-link large">
+                <div className="logo large"></div>
+              </Link>
+
+              <li className={`menu-nav_item ${menu ? "" : "open"}`}>
+                <Link to="#" className="menu-nav_link active">
+                  Home
+                </Link>
+              </li>
+              <li className={`menu-nav_item ${menu ? "" : "open"}`}>
+                <Link to="#about" className="menu-nav_link">
+                  About
+                </Link>
+              </li>
+              <li className={`menu-nav_item ${menu ? "" : "open"}`}>
+                <Link to="#services" className="menu-nav_link">
+                  Services
+                </Link>
+              </li>
+              <li className={`menu-nav_item ${menu ? "" : "open"}`}>
+                <Link to="#blog" className="menu-nav_link">
+                  Blog
+                </Link>
+              </li>
+              <li className={`menu-nav_item ${menu ? "" : "open"}`}>
+                <Link to="#contact" className="menu-nav_link">
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        {/* Nav Bar */}
+        {/* <nav id="navbar">
+          <Link to="#" className="logo-link">
+            <div className="logo"></div>
+          </Link> */}
+
+        {/* Tablet and Large Screen Menus */}
+        {/* <div className="lg-nav-menu">
             <ul id="lg-nav">
               <li className="lg-nav-menu-item">
                 <Link to="#" className="lg-nav-menu-item-link">
@@ -51,17 +106,17 @@ const NavBar = () => {
                   Contact
              </Link>
               </li>
-            </ul>
+            </ul> */}
 
-            {/* Menu Toggler */}
-            <div id="toggle-on" onClick={openMenu}>&#9776;</div>
+        {/* Menu Toggler */}
+        {/* <div id="toggle-on" onClick={openMenu}>&#9776;</div>
           </div>
-        </nav>
+        </nav> */}
 
         {/* Mobile Menu */}
-        <div id="sm-nav-menu" className={`${menu ? "" : "hide-menu"} `} >
-          {/* Menu Toggler */}
-          <div id="toggle-off" onClick={closeMenu}>&times;</div>
+        {/* <div id="sm-nav-menu" className={`${menu ? "" : "hide-menu"} `} > */}
+        {/* Menu Toggler */}
+        {/* <div id="toggle-off" onClick={closeMenu}>&times;</div>
           <ul id="sm-nav">
             <li className="sm-nav-menu-item">
               <Link to="#" className="sm-nav-menu-item-link">
@@ -89,7 +144,7 @@ const NavBar = () => {
            </Link>
             </li>
           </ul>
-        </div>
+        </div> */}
       </>
     );
   }
