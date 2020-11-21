ac3bebc3dff849a62357d869e0b49fa6a9b99189
prince <princeakpabio90@gmail.com>
2020-11-21 14:33:32 +0100

    removed errors

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -2,12 +2,12 @@ import React, {useRef} from "react";
 
 import Banner from "../../components/banner/banner";
 // import Carouselll from "../../components/Test2/carousel/carousell";
-import Intro from "../../components/intro/intro";
-import Services from "../../components/services/services";
-import About from "../../components/about/about";
-import Quiz from "../../components/quiz/quiz";
-import CallToAction from "../../components/callToAction/callToAction";
-import ContactLinks from "../../components/contactLinks/contactLinks";
+// import Intro from "../../components/intro/intro";
+// import Services from "../../components/services/services";
+// import About from "../../components/about/about";
+// import Quiz from "../../components/quiz/quiz";
+// import CallToAction from "../../components/call-to-action/callToAction";
+// import ContactLinks from "../../components/contact-links/contact-links";
 // import Modal from "../../components/modal/modal";
 import { useRouteMatch } from 'react-router-dom';
 
@@ -25,13 +25,13 @@ function Home() {
     <div className="home">
       {/* <Modal ref={modalRef}/> */}
       <Banner match={match} openModal={openModal} />
-      <Intro />
+      {/* <Intro />
       <Services />
-      <About />
+      <About /> */}
       {/* <Carouselll /> */}
-      <Quiz />
+      {/* <Quiz />
       <CallToAction />
-      <ContactLinks />
+      <ContactLinks /> */}
     </div>
   );
 }
