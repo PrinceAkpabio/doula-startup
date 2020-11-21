dc61a6f98f28467807a521160da36c4eb0a3639b
prince <princeakpabio90@gmail.com>
2020-11-21 13:30:31 +0100

    removed warnings

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Banner from '../../components/banner/banner';
-import Footer from '../../components/footer/footer';
 import Intro from '../../components/intro/intro';
 import Services from '../../components/services/services';
 import About from '../../components/about/about';
